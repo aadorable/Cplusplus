57c0c31cf4478497818e7ada319d2d42c67bbc95
aadorable <1615691183@qq.com>
2018-07-03 22:35:41 +0800

    AVLTree/

---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	K _key;
+	V _value;
+	int _bf;       //平衡因子，右子树高度减去左子树高度
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+
+	AVLTreeNode(K key, V value)
+		:_key(key)
+		, _value(value)
+		, _bf(0)
+		, _left(NULL)
+		, _right(NULL)
+		, _parent(NULL)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	AVLTree()
+		:_root(NULL)
+	{}
+	AVLTree(const AVLTree<K, V>& tree)
+		:_root(NULL)
+	{
+		_Copy(_root, tree._root);
+	}
+	AVLTree<K, V>& operator=(const AVLTree<K, V>& tree)
+	{
+		if (this != &tree)
+		{
+			AVLTree<K, V> tmp(tree);
+			swap(_root, tmp._root)
+		}
+		return *this;
+	}
+	~AVLTree()
+	{
+		_Destroy(_root);
+	}
+
+protected:
+	void _Copy(Node* root, Node* new_root)
+	{
+
+	}
+private:
+	Node* _root;
+};
