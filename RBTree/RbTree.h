a54d3fd8dca5ea43e5ee1b775026426b3b967677
aadorable <1615691183@qq.com>
2018-07-31 23:48:34 +0800

    add Test

--- RBTree/RbTree.h
+++ RBTree/RbTree.h
@@ -322,3 +322,21 @@ protected:
 private:
 	Node* _root;
 };
+
+void TestRBTree()
+{
+	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	RBTree<int, int> t;
+	for (size_t i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
+	{
+		t.Insert(a[i], i);
+		cout << a[i] << ":" << t.IsBalance() << endl;
+	}
+	t.InOrder();
+	cout << t.IsBalance() << endl;
+	cout << t.Find(16) << endl;
+	cout << t.Find(10) << endl;
+	t.Remove(11);
+	t.InOrder();
+	cout << t.IsBalance() << endl;
+}
