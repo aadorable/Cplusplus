297097da4c6667fbb79770b7d9f7524ecd3a4813
aadorable <1615691183@qq.com>
2018-08-01 22:23:53 +0800

    add Remove

--- RBTree/RbTree.h
+++ RBTree/RbTree.h
@@ -153,6 +153,229 @@ public:
 		_root->_color = BLACK;               //强制将根节点变为黑色
 		return true;
 	}
+	bool Remove(const K& key)
+	{
+		Node* cur = _root;
+		Node* parent = NULL;
+		Node* del = NULL;
+		while (cur)                 //寻找待删除结点
+		{
+			if (cur->_key > key)
+			{
+				cur = cur->_left;
+			}
+			else if (cur->_key < key)
+			{
+				cur = cur->_right;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (cur == NULL)
+		{
+			return false;                //要删除节点不存在，删除失败
+		}
+		del = cur;
+		//如果要删除的结点有两个孩子，则需找到右子树的最左结点
+		if (cur->_left != NULL && cur->_right != NULL)
+		{
+			cur = cur->_right;
+			while (cur->_left)
+			{
+				cur = cur->_left;
+			}
+			del->_key = cur->_key;
+			del->_value = cur->_value;
+			del = cur;                  //交换后使del指向要删除的结点
+		}
+		parent = cur->_parent;           //找到要删除节点的父亲
+		if (cur->_left == NULL)          //要删除节点的左孩子为空，或者都为空
+		{ 
+			if (parent == NULL)          //要删除的是头结点
+			{
+				_root = cur->_right;
+				if (cur->_right)
+				{
+					_root->_parent = NULL;
+					_root->_color = BLACK;        //记得根节点永远是黑色
+				}
+				delete del;
+				return true;
+			}
+			else
+			{
+				if (parent->_left == cur)
+				{
+					parent->_left = cur->_right;
+				}
+				else
+				{
+					parent->_right = cur->_right;
+				}
+				if (cur->_right)
+				{
+					cur->_right->_parent = parent;
+				}
+			}
+			cur = del->_right;               //cur更新到要删除节点的右子树
+		}
+		else                                //要删除节点的右孩子为空
+		{
+			if (parent == NULL)              //要删除节点为根节点
+			{
+				_root = cur->_left;
+				_root->_parent = NULL;
+				_root->_color = BLACK;
+				delete del;
+				return true;
+			}
+			else
+			{
+				if (parent->_left == cur)
+				{
+					parent->_left = cur->_left;
+				}
+				else
+				{
+					parent->_right = cur->_left;
+				}
+				cur->_left->_parent = parent;
+			}
+			cur = del->_left;
+		}
+
+		//根据颜色判断删除后是否还是合法的红黑树，若不合法则旋转
+		if (del->_color == RED)            //要删除结点是红色，则删除后任然平衡，无需旋转
+		{
+			delete del;
+			return true;
+		}
+		if (del->_color == BLACK && cur && cur->_color == RED)     //如果要删除节点是黑色，且它的孩子是红色			
+		{														//则将孩子变为黑色即可
+			cur->_color = BLACK;
+			delete del;
+			return true;
+		}
+		//要删除节点是黑色，且它的孩子为NULL或为黑色
+		while (parent)
+		{
+			if (parent->_left == cur)           //要删除节点是父节点的左孩子
+			{
+				Node* subR = parent->_right;
+				if (subR->_color == RED)
+				{
+					RotateL(parent);            //对parent左旋，染色
+					subR->_color = BLACK;
+					parent->_color = RED;
+				}
+				else                           //subR是黑色的
+				{
+					Node* subRL = subR->_left;
+					Node* subRR = subR->_right;
+					if (parent->_color == BLACK && (subRL == NULL && subRR == NULL) ||
+						(subRL && subRL->_color == BLACK && subRR && subRR->_color == BLACK))
+					{
+						subR->_color = RED;     //使subR这条路径上减少一个黑色结点，再向上判断
+						cur = parent;
+						parent = cur->_parent;
+					}
+					else
+					{
+						if (parent->_color == RED)
+						{
+							if ((subRL == NULL && subRR == NULL) ||
+								(subRL && subRL->_color == BLACK && subRR && subRR->_color == BLACK))
+							{
+								parent->_color = BLACK;     //将父节点变为黑色
+								subR->_color = RED;
+								break;
+							}
+						}
+						if (subRL->_color == RED)           //subRL为红色，先对subR进行右旋转换为左单旋情况
+						{
+							RotateR(subR);
+							subR = subRL;
+						}
+						RotateL(parent);
+						//将旋转后新的父节点变为与原来父节点一样的颜色
+						if (parent->_color == RED)         
+						{
+							subR->_color = RED;
+						}
+						else
+						{
+							subR->_color = BLACK;
+						}
+						//将原来的父节点染黑
+						parent->_color = BLACK;
+						subR->_right->_color = BLACK;       //由于subR的右子树少一个黑结点
+						break;
+					}
+				}
+			}
+			else                                          //要删除节点是父节点的右孩子
+			{
+				Node* subL = parent->_left;
+				if (subL->_color == RED)
+				{
+					RotateR(parent);                       //对parent右旋，染色
+					parent->_color = RED;
+					subL->_color = BLACK;
+				}
+				else                                      //parent的左孩子是黑的
+				{
+					Node* subLR = subL->_right;
+					Node* subLL = subL->_left;
+					//如果父节点和subL都是黑的，subL的孩子要么全空，要么全黑
+					if (parent->_color == BLACK && (subLL == NULL && subLR == NULL) ||
+						(subLL && subLL->_color == BLACK && subLR && subLR->_color == BLACK))
+					{
+						subL->_color = RED;               //使subL这条路径上减少一个黑色结点，再向上判断
+						cur = parent;
+						parent = cur->_parent;
+					}
+					else
+					{
+						if (parent->_color == RED)
+						{
+							if ((subLL == NULL && subLR == NULL) ||
+								(subLL && subLL->_color == BLACK && subLR && subLR->_color == BLACK))
+							{
+								parent->_color = BLACK;            //cur路径增加一个黑节点
+								subL->_color = RED;                //subL路径黑节点个数不变
+								break;
+							}
+						}
+						if (subLR->_color == RED)                  //subLR为红色，先对subL进行左旋转换为右单旋情况
+						{
+							RotateL(subL);
+							subL = subLR;
+						}
+						RotateR(parent);
+						//将旋转后新的父节点变为与原来父节点一样的颜色
+						if (parent->_color == RED)
+						{
+							subL->_color = RED;
+						}
+						else
+						{
+							subL->_color = BLACK;
+						}
+						//将原来的父节点染黑
+						parent->_color = BLACK;
+						subL->_left->_color = BLACK;
+						break;
+					}
+				}
+			}
+		}
+
+		_root->_color = BLACK;
+		delete del;
+		return true;
+	}
 	bool Find(const K& key)
 	{
 		Node* cur = _root;
