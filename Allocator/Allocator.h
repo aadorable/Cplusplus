392554bf1cfd44f3c0eb08a730b1ef1de13597b6
aadorable <1615691183@qq.com>
2018-08-13 23:25:05 +0800

    add SimpleAlloc

--- Allocator/Allocator.h
+++ Allocator/Allocator.h
@@ -241,3 +241,36 @@ void TestAlloc2()
 
 	__DefaultAllocTemplate<false, 0>::Allocate(6);
 }
+
+
+#ifdef __USE_MALLOC
+typedef __MallocAllocTemplate<0> Alloc;
+#else
+typedef __DefaultAllocTemplate<false, 0> Alloc;
+#endif
+
+
+template<class T, class Alloc>
+class SimpleAlloc {
+public:
+	static T* Allocate(size_t n)
+	{
+		return 0 == n ? 0 : (T*)Alloc::Allocate(n * sizeof (T));
+	}
+
+	static T* Allocate(void)
+	{
+		return (T*)Alloc::Allocate(sizeof (T));
+	}
+
+	static void Deallocate(T *p, size_t n)
+	{
+		if (0 != n)
+			Alloc::Deallocate(p, n * sizeof (T));
+	}
+
+	static void Deallocate(T *p)
+	{
+		Alloc::Deallocate(p, sizeof (T));
+	}
+};
