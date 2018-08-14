08b00795d7189adc53d3705e25997d3ae28f1a86
aadorable <1615691183@qq.com>
2018-08-14 23:58:42 +0800

    add GetNextPrime

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -86,6 +86,28 @@ public:
 		return Iterator(NULL, this);
 	}
 
+	size_t GetNextPrime(size_t cur)
+	{
+		const int _PrimeSize = 28;
+		static const unsigned long _PrimeList[_PrimeSize] =
+		{
+			53ul,	         97ul,         193ul,       389ul,       769ul,
+			1543ul,        3079ul,       6151ul,      12289ul,     24593ul,
+			49157ul,       98317ul,      196613ul,    393241ul,    786433ul,
+			1572869ul,     3145739ul,    6291469ul,   12582917ul,  25165843ul,
+			50331653ul,	  100663319ul,  201326611ul, 402653189ul, 805306457ul,
+			1610612741ul, 3221225473ul, 4294967291ul
+		};
+		for (size_t i = 0; i < _PrimeSize; ++i)
+		{
+			if (_PrimeList[i] > cur)
+			{
+				return _PrimeList[i];
+			}
+		}
+		return _PrimeList[_PrimeSize - 1];
+	}
+
 	void CheckCapacity()
 	{
 		if (_size == _tables.size())       // load factor == 1
