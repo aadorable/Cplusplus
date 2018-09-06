938bb63e71b9a7594e9e4f8879611969f5992ace
aadorable <1615691183@qq.com>
2018-09-06 23:25:42 +0800

    add TestMapSize

--- gtest/map.cc
+++ gtest/map.cc
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <map>
 
 /////////////////////////////////////////////////////////////////
 //该代码对 std::map 常用接口进行测试
 //事件机制充分的体现出“框架”含义
 //gtest 给我们提供了一组时机
 /////////////////////////////////////////////////////////////////
+void MakeMap(std::map<int, int>& test_map){
+    test_map.insert(std::make_pair(1, 1));
+    test_map.insert(std::make_pair(2, 1));
+    test_map.insert(std::make_pair(3, 1));
+    test_map.insert(std::make_pair(4, 1));
+    test_map.insert(std::make_pair(5, 1));
+}
+
+TEST(TestMap, size){
+    std::map<int, int> test_map;
+    MakeMap(test_map);
+    ASSERT_EQ(test_map.size(), 5);
+}
